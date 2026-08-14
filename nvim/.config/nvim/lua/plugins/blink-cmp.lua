962c5cc38a2052166dec1b000b1e0a186e149399
Kenny Sheldon <kenny.sheldon99@gmail.com>
2026-08-13 22:28:04 -0500

    border

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -47,7 +47,10 @@ return {
 			},
 		},
 
-		signature = { enabled = true },
+		signature = {
+			enabled = true,
+			window = { border = "rounded" },
+		},
 
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
